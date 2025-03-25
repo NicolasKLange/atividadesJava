6dd92f4d5d92ad44871f3515f29c9b1a5e355775
nicolasKLange <nklange986@gmail.com>
2025-03-25 13:59:26 -0300

    Media de notas, quantidade de notas informada  pelo usuario

--- src/MediaNotas.java
+++ src/MediaNotas.java
@@ -4,13 +4,17 @@ public class MediaNotas {
     public static void main(String[] args) {
         // Extanciando Scanner para coletade dados
         Scanner scanner = new Scanner(System.in);
-        // Vetor para armazenar as notas
+        // Exemplo de vetor para armazenar 4 notas
         double[] notas = new double[4];
         // somador das notas informadas
         double soma = 0;
 
+        // Entrada de dados, quantidades de notas para fazer media
+        System.out.print("Informe a quantidade de notas: ");
+        int quantidadeNotas = scanner.nextInt();
+
         // Entrada de dados, notas
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < quantidadeNotas; i++) {
             System.out.print("Informe a " + (i + 1) + "° nota: ");
             // Armazena as notas no vetor de notas
             notas[i] = scanner.nextDouble();
@@ -19,8 +23,8 @@ public class MediaNotas {
             soma += notas[i];
         }
 
-        // Calculando a média das 4 notas
-        double media = soma/4;
+        // Calculando a média das notas informadas pelo usuário
+        double media = soma/ quantidadeNotas;
 
         System.out.println("Média: " + media);
 
