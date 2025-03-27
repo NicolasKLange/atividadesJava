b63976efbb5224fda3ce60f170965c44015d59a2
nicolasKLange <nklange986@gmail.com>
2025-03-27 18:08:08 -0300

    lista de compras

--- src/listaDeCompras.java
+++ src/listaDeCompras.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class listaDeCompras {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        char sair = 'n';
+        int contador = 1;
+
+        while (sair == 'n') {
+            System.out.print("Informe o "+ contador +"° produto: ");
+            String[] produtos = new String[contador];
+            
+            System.out.println("Deseja sair?");
+            char saida = Character.toUpperCase(scanner.next().charAt(0));
+            contador ++;
+        }
+        
         
     }
 }
